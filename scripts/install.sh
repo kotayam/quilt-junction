afcd8b6bdcd4f73541b62c283919062c03f8b5a7
kotayam <ykomako4.9@gmail.com>
2025-10-15 19:43:26 -0400

    dont install function bench in ci mode

--- scripts/install.sh
+++ scripts/install.sh
@@ -30,12 +30,15 @@ cd $SCRIPT_DIR
 ./install_cereal.sh
 ./install_cmake.sh
 ./install_flatbuffers.sh
-./install_function_bench.sh
 ./install_glibc.sh
 
 if [ "$CI_MODE" = true ]; then
     ./install_caladan.sh --ci
 else
     ./install_caladan.sh
+    ./install_function_bench.sh
 fi
+
+
+
 
