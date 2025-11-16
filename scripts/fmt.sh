0720bd0b2bc9ae8b36298796705d922a6326a7b0
kotayam <ykomako4.9@gmail.com>
2025-11-16 12:12:57 -0500

    use clang format v20

--- scripts/fmt.sh
+++ scripts/fmt.sh
@@ -9,7 +9,7 @@ CODE_DIR=${ROOT_DIR}/junction
 
 # fmt C++
 # style is based on Google's style, defined in the .clang-format file
-find "${CODE_DIR}" -iname '*.h' -o -iname '*.cc' | xargs clang-format -i
+find "${CODE_DIR}" -iname '*.h' -o -iname '*.cc' | xargs clang-format-20 -i
 
 # fmt Python
 #yapf3 --style pep8 -i scripts/benchmark.py
--- scripts/fmt_check.sh
+++ scripts/fmt_check.sh
@@ -8,7 +8,7 @@ FORMAT_PATH=${ROOT_DIR}/.clang-format
 
 # check format for C++
 # style is based on Google's style, defined in the .clang-format file
-find "${CODE_DIR}" -iname '*.h' -o -iname '*.cc' | xargs clang-format -style=file:"${FORMAT_PATH}" --dry-run -Werror
+find "${CODE_DIR}" -iname '*.h' -o -iname '*.cc' | xargs clang-format-20 -style=file:"${FORMAT_PATH}" --dry-run -Werror
 
 # fmt Python
 #yapf3 --style pep8 -i scripts/benchmark.py
