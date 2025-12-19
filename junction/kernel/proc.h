8d77baa0cb9fe01592b77394935a9bc4c1b0ae48
kotayam <ykomako4.9@gmail.com>
2025-12-19 17:01:50 +0000

    fix build err

--- junction/kernel/proc.h
+++ junction/kernel/proc.h
@@ -882,7 +882,8 @@ class Process : public std::enable_shared_from_this<Process> {
     if (!cwd) throw std::runtime_error("stale cwd during snapshot");
     ar(*cwd);
 
-    ar(thread_map_.size(), GetLastBlockedTid(GetCfg().GetArg("function_name")));
+    std::string function_name = GetCfg().GetArg("function_name");
+    ar(thread_map_.size(), GetLastBlockedTid(function_name));
     for (const auto &[pid, th] : thread_map_) {
       ar(pid);
       th->DoSave(ar);
