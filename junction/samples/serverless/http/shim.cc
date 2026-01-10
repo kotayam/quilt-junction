2e185807cf89fa80c26b4dc876296b88f44c1959
kotayam <ykomako4.9@gmail.com>
2026-01-10 08:05:04 +0000

    add logs to shim

--- junction/samples/serverless/http/shim.cc
+++ junction/samples/serverless/http/shim.cc
@@ -3,11 +3,12 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 
+#include <cstdio>
 #include <cstring>
-#include <iostream>
 
 constexpr const char *GATEWAY_IP = "10.10.1.1";
 constexpr int GATEWAY_PORT = 8080;
+constexpr const char *SIDECAR_IP = "127.0.0.1";
 constexpr int SIDECAR_PORT = 9000;
 
 using connect_func_t = int (*)(int, const struct sockaddr *, socklen_t);
@@ -18,6 +19,11 @@ extern "C" int connect(int sockfd, const struct sockaddr *addr,
   static connect_func_t real_connect = nullptr;
   if (!real_connect) {
     real_connect = (connect_func_t)dlsym(RTLD_NEXT, "connect");
+    if (!real_connect) {
+      fprintf(stderr,
+              "[Shim] Critical Error: dlsym failed to find 'connect'\n");
+      return -1;
+    }
   }
   if (addr->sa_family == AF_INET) {
     const auto *addr_in = (const struct sockaddr_in *)addr;
@@ -27,14 +33,16 @@ extern "C" int connect(int sockfd, const struct sockaddr *addr,
 
     if (strcmp(ip_str, GATEWAY_IP) == 0 && port == GATEWAY_PORT) {
       // intercept if it is trying to connect to the gateway
-      std::cout << "[Shim] Intercepting connection to gateway and redirecting "
-                   "to sidecar"
-                << std::endl;
+      fprintf(stderr, "[Shim] Intercepting %s:%d -> Redirecting to %s:%d\n",
+              ip_str, port, SIDECAR_IP, SIDECAR_PORT);
       struct sockaddr_in sidecar_addr;
       memset(&sidecar_addr, 0, sizeof(sidecar_addr));
       sidecar_addr.sin_family = AF_INET;
       sidecar_addr.sin_port = htons(SIDECAR_PORT);
-      inet_pton(AF_INET, "0.0.0.0", &sidecar_addr.sin_addr);
+      if (inet_pton(AF_INET, SIDECAR_IP, &sidecar_addr.sin_addr) <= 0) {
+        fprintf(stderr, "[Shim] Error: Failed to convert Sidecar IP\n");
+        return -1;
+      }
       return real_connect(sockfd, (struct sockaddr *)&sidecar_addr,
                           sizeof(sidecar_addr));
     }
