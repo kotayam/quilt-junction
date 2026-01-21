d87577cd13ab4d0b67a66d23fb6589c0d54f1550
kotayam <ykomako4.9@gmail.com>
2026-01-21 03:04:46 +0000

    add delay in client

--- junction/samples/serverless/http/client.py
+++ junction/samples/serverless/http/client.py
@@ -55,6 +55,7 @@ def run_test_suite(host, port, request_count=1000):
         # Warm up with a simple request
         for _ in range(20):
             send_measured_request(conn, "GET", "/followers/1")
+            time.sleep(0.01)
         print(" done.")
     except Exception as e:
         print(f"\n[!] Warm-up failed: {e}")
@@ -74,6 +75,7 @@ def run_test_suite(host, port, request_count=1000):
             # --- USE HELPER ---
             latency, resp, _ = send_measured_request(conn, "GET", path)
             latencies.append(latency)
+            time.sleep(0.01)
             
             if resp.status != 200:
                 print(f"!", end="") # Visual indicator of errors
