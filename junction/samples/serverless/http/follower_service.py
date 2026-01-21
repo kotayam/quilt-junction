ff44433348cdc1935d4a1ee6766b157cd6e7938a
kotayam <ykomako4.9@gmail.com>
2026-01-21 00:41:48 +0000

    manual patch

--- junction/samples/serverless/http/follower_service.py
+++ junction/samples/serverless/http/follower_service.py
@@ -2,9 +2,20 @@ import http.client
 from http.server import BaseHTTPRequestHandler
 import sys
 from watchdog import WatchDog
-import patch
 
-patch.patch_socket()
+import socket
+
+def patch_socket():
+    _original_setsockopt = socket.socket.setsockopt
+
+    def patched_setsockopt(self, level, optname, value):
+        try:
+            _original_setsockopt(self, level, optname, value)
+        except OSError:
+            # Ignore "Invalid argument" or "Protocol not available" from the runtime
+            pass
+
+    socket.socket.setsockopt = patched_setsockopt
 
 # --- 1. CONFIGURATION ---
 GATEWAY_IP = "10.10.1.1"
