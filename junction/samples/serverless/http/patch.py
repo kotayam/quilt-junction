1dfac5f12548352b67354ca0848aebf29057e2d2
kotayam <ykomako4.9@gmail.com>
2026-01-21 00:43:49 +0000

    completely ignore setsockopt py

--- junction/samples/serverless/http/patch.py
+++ junction/samples/serverless/http/patch.py
@@ -1,13 +1,7 @@
 import socket
 
 def patch_socket():
-    _original_setsockopt = socket.socket.setsockopt
-
-    def patched_setsockopt(self, level, optname, value):
-        try:
-            _original_setsockopt(self, level, optname, value)
-        except OSError:
-            # Ignore "Invalid argument" or "Protocol not available" from the runtime
-            pass
+    def noop_setsockopt(self, level, optname, value):
+        pass
 
     socket.socket.setsockopt = patched_setsockopt
