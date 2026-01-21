3b617c795f7ce31ba1992fdd9f30cc58aec0a03a
kotayam <ykomako4.9@gmail.com>
2026-01-21 02:19:04 +0000

    keep alive gateway

--- junction/samples/serverless/http/gateway.cc
+++ junction/samples/serverless/http/gateway.cc
@@ -42,7 +42,7 @@ void ProxyHandler(const httplib::Request &req, httplib::Response &res) {
             << req.path << std::endl;
 
   httplib::Client cli(CONTROLLER_IP, CONTROLLER_PORT);
-  cli.set_keep_alive(false);
+  cli.set_keep_alive(true);
   // cli.set_connection_timeout(0, TIMEOUT_US);
 
   httplib::Result ctrl_res;
